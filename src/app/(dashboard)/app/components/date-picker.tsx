9d21e92faf5788e35f8208d537ea71d3ec10448f
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2025-01-23 13:06:45 +0900

    fix: todayページでも期限を"今日"以外にもできるように変更

--- src/app/(dashboard)/app/components/date-picker.tsx
+++ src/app/(dashboard)/app/components/date-picker.tsx
@@ -4,7 +4,7 @@ import { format } from 'date-fns';
 import { ja } from 'date-fns/locale';
 import { CalendarIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -24,58 +24,40 @@ export const DatePicker = () => {
     isTodayPage ? new Date() : undefined
   );
 
-  useEffect(() => {
-    if (isTodayPage) {
-      setDate(new Date());
-    }
-  }, [isTodayPage]);
-
   return (
     <>
-      {isTodayPage ? (
-        <div className="flex items-center space-x-2">
-          <CalendarIcon />
-          <span>{format(new Date(), 'yyyy年MM月dd日', { locale: ja })}</span>
-          <input
-            type="hidden"
-            name="deadline"
-            value={new Date().toISOString()}
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={'outline'}
+            className={cn(
+              'justify-start text-left font-normal',
+              !date && 'text-muted-foreground'
+            )}
+            aria-label="日付を選択"
+          >
+            <CalendarIcon />
+            {date ? (
+              format(date, 'yyyy年MM月dd日', { locale: ja })
+            ) : (
+              <span>期限</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
           />
-        </div>
-      ) : (
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={'outline'}
-              className={cn(
-                'justify-start text-left font-normal',
-                !date && 'text-muted-foreground'
-              )}
-              aria-label="日付を選択"
-            >
-              <CalendarIcon />
-              {date ? (
-                format(date, 'yyyy年MM月dd日', { locale: ja })
-              ) : (
-                <span>期限</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              initialFocus
-            />
-          </PopoverContent>
-          <input
-            type="hidden"
-            name="deadline"
-            value={date ? new Date(date).toISOString() : ''}
-          />
-        </Popover>
-      )}
+        </PopoverContent>
+        <input
+          type="hidden"
+          name="deadline"
+          value={date ? new Date(date).toISOString() : ''}
+        />
+      </Popover>
     </>
   );
 };
